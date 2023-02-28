c04bcc4ebe4e57c9a7911ef8ac26ff88ba2f2c12
leandrosconceicao <leandrosantosconceicao@gmail.com>
2023-02-27 23:06:35 -0300

    Novos metodos

--- lib/src/utils/forms/ingredients_form.dart
+++ lib/src/utils/forms/ingredients_form.dart
@@ -1,5 +1,6 @@
 import 'package:bakery/libraries/views.dart';
 
+import '../../../libraries/models.dart';
 import '/libraries/utils.dart';
 
 class IngredientsForm {
@@ -7,6 +8,11 @@ class IngredientsForm {
   final qtdPackage = TextEditingController();
   final name = TextEditingController();
   final unit = TextEditingController();
+
+  final costFcs = FocusNode();
+  final qtdFcs = FocusNode();
+  final nameFcs = FocusNode();
+  final unitFcs = FocusNode();
 
   num getCost() {
     return num.tryParse(cost.text.replaceAll(',', '.')) ?? 0.0;
@@ -22,6 +28,20 @@ class IngredientsForm {
 
   String getUnit() {
     return unit.text;
+  }
+
+  void fillForm(Ingredients? data) {
+    cost.text = data?.cost?.toStringAsFixed(2) ?? '';
+    qtdPackage.text = data?.quantityInPackage.toString() ?? '';
+    name.text = data?.name ?? '';
+    unit.text = data?.unitOfMeasurement ?? '';
+  }
+
+  void clear() {
+    cost.clear();
+    qtdPackage.clear();
+    name.clear();
+    unit.clear();
   }
 }
 
