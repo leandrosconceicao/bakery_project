5a713a8aad2fd67aac0d576d56db3bd0f2661067
leandrosconceicao <leandrosantosconceicao@gmail.com>
2023-03-01 23:38:26 -0300

    Ajuste no widget de layout padrão

--- lib/src/views/components/default_layout.dart
+++ lib/src/views/components/default_layout.dart
@@ -1,4 +1,7 @@
+import 'dart:isolate';
+
 import '../../../libraries/models.dart';
+import '../../../libraries/utils.dart';
 import '../../../libraries/views.dart';
 
 class DefaultPage extends StatelessWidget {
@@ -7,7 +10,7 @@ class DefaultPage extends StatelessWidget {
   final Widget? appBarTitle;
   final Widget? floatingAction;
   final bool? hideAppBar;
-  const DefaultPage({
+  DefaultPage({
     super.key,
     required this.title,
     required this.child,
@@ -16,17 +19,73 @@ class DefaultPage extends StatelessWidget {
     this.hideAppBar,
   });
 
+  final keyboardIsVisible = false.obs;
+
   @override
   Widget build(BuildContext context) {
     return Title(
       color: Get.theme.primaryColor,
       title: title,
-      child: Scaffold(
-        drawer: Get.currentRoute == Routes.signin ? null : const AppDrawer(),
-        appBar: (hideAppBar ?? false) ? null : MyAppBar.build(title: appBarTitle),
-        body: child,
-        floatingActionButton: floatingAction,
+      child: SafeArea(
+        child: Scaffold(
+          // drawer: Get.currentRoute == Routes.signin ? null : const AppDrawer(),
+          // appBar: (hideAppBar ?? false) ? null : MyAppBar.build(title: appBarTitle),
+          body: Stack(
+            children: [
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    child: Text(title, style: Get.textTheme.headlineMedium,),
+                  ),
+                  Expanded(child: child),
+                ],
+              ),
+              if (Get.currentRoute != Routes.signin) ... {
+                Obx(() => keyboardIsVisible.value ? const SizedBox() : Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Card(
+                    elevation: 8.0,
+                    color: MyPallete.defaultColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16.0)
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          menuItem(func: () => Get.toNamed(Routes.home), icon: Icons.home, route: Routes.home),
+                          menuItem(func: () => Get.toNamed(Routes.ingredients), icon: Icons.label, route: Routes.ingredients),
+                          menuItem(func: () => Get.toNamed(Routes.receipt), icon: Icons.receipt_long, route: Routes.receipt),
+                          menuItem(func: () => Get.toNamed(Routes.configs), icon: Icons.settings, route: Routes.configs),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ))
+              }
+            ],
+          ),
+          // floatingActionButton: floatingAction,
+        ),
       ),
+    );
+  }
+
+  Widget menuItem({
+    required void Function() func,
+    required IconData icon,
+    required String route,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: GestureDetector(
+        onTap: func,
+        child: Icon(icon, color: app.isActiveRoute(route) ? Colors.white : Colors.grey, size: app.isActiveRoute(route) ? (Get.height * 0.04) : (Get.height * 0.035),)),
     );
   }
 }
