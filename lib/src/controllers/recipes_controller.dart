13a7af4bed37d4ac46a4ad45942165beff9ae9d5
leandrosconceicao <leandrosantosconceicao@gmail.com>
2023-03-01 23:37:08 -0300

    Controlador de receitas

--- lib/src/controllers/recipes_controller.dart
+++ lib/src/controllers/recipes_controller.dart
@@ -1,26 +1,26 @@
 import '../../libraries/models.dart';
 import '../../libraries/utils.dart';
 
-class RecipesController {
-  static final bloc = StreamController<ApiRes<List<Recipes?>>?>.broadcast();
+class ReceiptsController {
+  static final bloc = StreamController<ApiRes<List<Receipts?>>>.broadcast();
 
   static Future<void> load({bool? clearValues = true}) async {
-    if (clearValues!) {
-      bloc.add(null);
-    }
+    // if (clearValues!) {
+    //   bloc.add(null);
+    // }
     bloc.add(await findAll());
   }
 
-  static Future<ApiRes<List<Recipes?>>> findAll() async {
-    final req = await Api.request<List<Recipes?>>(
+  static Future<ApiRes<List<Receipts?>>> findAll() async {
+    final req = await Api.request<List<Receipts?>>(
       method: ApiMethods.get,
       endpoint: '${Endpoints.bakeryRecipes}?storeCode=${app.storeCode}',
-      function: Recipes.toList,
+      function: Receipts.toList,
     );
     return req;
   }
 
-  static Future<ApiRes> post({Recipes? newData}) async {
+  static Future<ApiRes> post({Receipts? newData}) async {
     final req = await Api.request(
         method: ApiMethods.post,
         endpoint: Endpoints.bakeryRecipes,
@@ -28,16 +28,16 @@ class RecipesController {
     return req;
   }
 
-  static Future<ApiRes<List<Recipes?>>> delete({Recipes? data}) async {
-    final req = await Api.request<List<Recipes?>>(
+  static Future<ApiRes<List<Receipts?>>> delete({Receipts? data}) async {
+    final req = await Api.request<List<Receipts?>>(
         method: ApiMethods.del,
         endpoint: Endpoints.bakeryRecipes,
         body: {"_id": data?.id},
-        function: Recipes.toList);
+        function: Receipts.toList);
     return req;
   }
 
-  static Future<ApiRes> update({required String id, Recipes? data}) async {
+  static Future<ApiRes> update({required String id, Receipts? data}) async {
     final params = {
         "_id": id,
         "data": data?.toJson()
