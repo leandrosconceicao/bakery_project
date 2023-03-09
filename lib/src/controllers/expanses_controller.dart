b5bf840815798bfb208a8dcd97778c1bf6a0c16a
leandrosconceicao <leandrosantosconceicao@gmail.com>
2023-03-08 22:49:30 -0300

    Controlador para as despesas

--- lib/src/controllers/expanses_controller.dart
+++ lib/src/controllers/expanses_controller.dart
@@ -4,10 +4,9 @@ import '../../libraries/utils.dart';
 final expansesControl = ExpansesController();
 
 class ExpansesController {
-  
   final bloc = StreamController<ApiRes<List<Expanses?>>>.broadcast();
 
-  void load({bool? clearValues=true}) async {
+  void load({bool? clearValues = true}) async {
     // if (clearValues!) {
     //   bloc.add(null);
     // }
@@ -16,19 +15,42 @@ class ExpansesController {
 
   Future<ApiRes<List<Expanses?>>> findAll() async {
     final req = await Api.request<List<Expanses?>>(
-      method: ApiMethods.get,
-      endpoint: '${Endpoints.expanses}?storeCode=${app.storeCode}',
-      function: Expanses.toList
-    );
+        method: ApiMethods.get,
+        endpoint: '${Endpoints.bakeryExpanses}?storeCode=${app.storeCode}',
+        function: Expanses.toList);
     return req;
   }
 
-  Future<ApiRes> post() async {
+  Future<ApiRes> post({Expanses? newData}) async {
+    final map = newData?.toJson();
     final req = await Api.request(
-      method: ApiMethods.post,
-      endpoint: Endpoints.expanses,
+        method: ApiMethods.post, endpoint: Endpoints.bakeryExpanses, body: map);
+    return req;
+  }
+
+  Future<ApiRes<Expanses?>> delete({Expanses? data}) {
+    final map = {"id": data?.id};
+    final req = Api.request<Expanses?>(
+      method: ApiMethods.del,
+      endpoint: Endpoints.bakeryExpanses,
+      body: map,
     );
     return req;
   }
 
+  Future<ApiRes> update({String? id}) async {
+    final map = {
+      "id": id,
+      "data": {
+        "description": expanseForm.desc.text,
+        "value": expanseForm.getValue,
+      }
+    };
+    final req = await Api.request(
+      method: ApiMethods.put,
+      endpoint: Endpoints.bakeryExpanses,
+      body: map,
+    );
+    return req;
+  }
 }
