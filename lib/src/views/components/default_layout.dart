a946f7cb04d0831b2ecdf07ed136349174674b9a
leandrosconceicao <leandrosantosconceicao@gmail.com>
2023-03-08 22:50:28 -0300

    Refatoração

--- lib/src/views/components/default_layout.dart
+++ lib/src/views/components/default_layout.dart
@@ -36,38 +36,59 @@ class DefaultPage extends StatelessWidget {
                 children: [
                   Padding(
                     padding: const EdgeInsets.symmetric(vertical: 16.0),
-                    child: Text(title, style: Get.textTheme.headlineMedium,),
+                    child: Text(
+                      title,
+                      style: Get.textTheme.headlineMedium,
+                    ),
                   ),
                   Expanded(child: child),
                 ],
               ),
-              if (Get.currentRoute != Routes.signin) ... {
-                Obx(() => keyboardIsVisible.value ? const SizedBox() : Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Card(
-                    elevation: 8.0,
-                    color: MyPallete.defaultColor,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16.0)
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          menuItem(func: () => Get.toNamed(Routes.home), icon: Icons.home, route: Routes.home),
-                          menuItem(func: () => Get.toNamed(Routes.ingredients), icon: Icons.label, route: Routes.ingredients),
-                          menuItem(func: () => Get.toNamed(Routes.receipt), icon: Icons.receipt_long, route: Routes.receipt),
-                          menuItem(func: () => Get.toNamed(Routes.expenses), icon: Icons.bar_chart_rounded, route: Routes.expenses),
-                          menuItem(func: () => Get.toNamed(Routes.configs), icon: Icons.settings, route: Routes.configs),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ))
+              if (Get.currentRoute != Routes.signin) ...{
+                Obx(() => keyboardIsVisible.value
+                    ? const SizedBox()
+                    : Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Card(
+                            elevation: 8.0,
+                            color: MyPallete.defaultColor,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(16.0)),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  menuItem(
+                                      func: () => Get.toNamed(Routes.home),
+                                      icon: Icons.home,
+                                      route: Routes.home),
+                                  menuItem(
+                                      func: () =>
+                                          Get.toNamed(Routes.ingredients),
+                                      icon: Icons.label,
+                                      route: Routes.ingredients),
+                                  menuItem(
+                                      func: () => Get.toNamed(Routes.receipt),
+                                      icon: Icons.receipt_long,
+                                      route: Routes.receipt),
+                                  menuItem(
+                                      func: () => Get.toNamed(Routes.expenses),
+                                      icon: Icons.bar_chart_rounded,
+                                      route: Routes.expenses),
+                                  menuItem(
+                                      func: () => Get.toNamed(Routes.configs),
+                                      icon: Icons.settings,
+                                      route: Routes.configs),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ))
               }
             ],
           ),
@@ -82,12 +103,14 @@ class DefaultPage extends StatelessWidget {
     required IconData icon,
     required String route,
   }) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Ink(
-        child: InkWell(
-          onTap: func,
-          child: Icon(icon, color: app.isActiveRoute(route) ? Colors.white : Colors.black26, size: app.isActiveRoute(route) ? (Get.height * 0.04) : (Get.height * 0.035),)),
+    return IconButton(
+      onPressed: func,
+      icon: Icon(
+        icon,
+        color: app.isActiveRoute(route) ? Colors.white : Colors.black26,
+        size: app.isActiveRoute(route)
+            ? (Get.height * 0.04)
+            : (Get.height * 0.035),
       ),
     );
   }
